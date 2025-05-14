63493c953102f02e42719474ee0af25118ef59f7
Magi1l <456jkl632@gmail.com>
2025-05-14 11:54:13 +0900

    test(nextjs): not-found.tsx 임시 삭제로 라우트 트리 강제 리빌드 유도

--- src/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-  return (
-    <div style={{ padding: 40, textAlign: "center" }}>
-      <h1>404 - 페이지를 찾을 수 없습니다.</h1>
-      <p>요청하신 페이지가 존재하지 않거나, 이동되었을 수 있습니다.</p>
-    </div>
-  );
-} 
